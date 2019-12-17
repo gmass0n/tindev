7fcee16840a604fe44537bb1015849ce34698a53
Gabriel Masson <g.masson010@gmail.com>
2019-12-17 10:08:43 -0300

    ExpressJS back-end application

--- tindev-api/src/server.js
+++ tindev-api/src/server.js
@@ -4,7 +4,18 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {
+}
+
+io.on('connection', socket => {
+  const {user} = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
 
 // CONEXAO COM O BANCO DE DADOS
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-9vnrx.mongodb.net/omnistack8?retryWrites=true&w=majority', {
@@ -12,8 +23,15 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-9vnrx.mongodb.net/o
   useUnifiedTopology: true
 })
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
